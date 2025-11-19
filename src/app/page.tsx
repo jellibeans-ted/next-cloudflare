66abdc3e12bd9ad2be25bab1aef321116571c4da
jellibeans-ted <ted@jellibean.co>
2025-11-19 13:34:01 +0800

    chore: use next navigation instead of next router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
